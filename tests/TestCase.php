5ca5f8b46d8c0b06b4114aee05cc31a5eef1ff9e
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2020-07-30 12:04:15 +0200

    updates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,26 @@
 namespace AsayDev\LaraTickets\Tests;
 
 
+use AsayDev\LaraTickets\AsayDevLaraTicketsServiceProvider;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
 
+    protected function getPackageProviders($app)
+    {
+        return [
+            AsayDevLaraTicketsServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
 
 }

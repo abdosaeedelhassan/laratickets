22a444715252768adbefe3f3b0a55af659143603
Abdelrahman Saeed <abdosaeedelhassan@gmail.com>
2021-04-01 09:45:51 +0200

    updates

--- src/Livewire/Components/Tickets/LaraTicketsForm.php
+++ src/Livewire/Components/Tickets/LaraTicketsForm.php
@@ -179,7 +179,6 @@ class LaraTicketsForm extends Component
 
     public function goback()
     {
-        dd( $this->dashboardData);
         $this->dashboardData['form'] = ['name' => '', 'action' => '', 'id' => ''];
         $this->emit('activeNvTab', $this->dashboardData);
     }

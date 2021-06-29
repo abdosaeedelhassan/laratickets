b0aa7c43b5069b7b214cc1f040d25edaba65518f
Abdelrahman Saeed <abdosaeedelhassan@gmail.com>
2021-06-29 13:33:28 +0200

    updates

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -34,38 +34,49 @@
         </li>
     @endif
     @if($dashboardData['usertype']=='admin')
-        <li  class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle {{in_array($dashboardData['active_nav_tab'],['statuses-tab','priorities-tab','agents-tab','config-tab','category-tab','admin-tab'])?'active':''}}"
-               data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                {{ trans('laratickets::admin.nav-settings') }}
-                @if($dashboardData['active_nav_tab']=='statuses-tab')
-                    : {{ trans('laratickets::admin.nav-statuses') }}
-                @elseif($dashboardData['active_nav_tab']=='priorities-tab')
-                    {{ trans('laratickets::admin.nav-priorities') }}
-                @elseif($dashboardData['active_nav_tab']=='agents-tab')
-                    {{ trans('laratickets::admin.nav-agents') }}
-                @elseif($dashboardData['active_nav_tab']=='category-tab')
-                    {{ trans('laratickets::admin.nav-categories') }}
-                @elseif($dashboardData['active_nav_tab']=='config-tab')
-                    {{ trans('laratickets::admin.nav-configuration') }}
-                @elseif($dashboardData['active_nav_tab']=='admin-tab')
-                    {{ trans('laratickets::admin.nav-administrator') }}
-                @endif
-            </a>
-            <div class="dropdown-menu">
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='statuses-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('statuses-tab')">{{ trans('laratickets::admin.nav-statuses') }}</a>
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='priorities-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('priorities-tab')">{{ trans('laratickets::admin.nav-priorities') }}</a>
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='agents-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('agents-tab')">{{ trans('laratickets::admin.nav-agents') }}</a>
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='category-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('category-tab')">{{ trans('laratickets::admin.nav-categories') }}</a>
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='config-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('config-tab')">{{ trans('laratickets::admin.nav-configuration') }}</a>
-                <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='admin-tab' ? "active" : "" !!}"
-                   wire:click="setActiveNavTab('admin-tab')">{{ trans('laratickets::admin.nav-administrator') }}</a>
-            </div>
-        </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle {{in_array($dashboardData['active_nav_tab'],['statuses-tab','priorities-tab','agents-tab','config-tab','category-tab','admin-tab'])?'active':''}}" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ trans('laratickets::admin.nav-settings') }}
+                    @if($dashboardData['active_nav_tab']=='statuses-tab')
+                        : {{ trans('laratickets::admin.nav-statuses') }}
+                    @elseif($dashboardData['active_nav_tab']=='priorities-tab')
+                        {{ trans('laratickets::admin.nav-priorities') }}
+                    @elseif($dashboardData['active_nav_tab']=='agents-tab')
+                        {{ trans('laratickets::admin.nav-agents') }}
+                    @elseif($dashboardData['active_nav_tab']=='category-tab')
+                        {{ trans('laratickets::admin.nav-categories') }}
+                    @elseif($dashboardData['active_nav_tab']=='config-tab')
+                        {{ trans('laratickets::admin.nav-configuration') }}
+                    @elseif($dashboardData['active_nav_tab']=='admin-tab')
+                        {{ trans('laratickets::admin.nav-administrator') }}
+                    @endif
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='statuses-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('statuses-tab')">{{ trans('laratickets::admin.nav-statuses') }}</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='priorities-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('priorities-tab')">{{ trans('laratickets::admin.nav-priorities') }}</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='agents-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('agents-tab')">{{ trans('laratickets::admin.nav-agents') }}</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='category-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('category-tab')">{{ trans('laratickets::admin.nav-categories') }}</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='config-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('config-tab')">{{ trans('laratickets::admin.nav-configuration') }}</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item {!! $dashboardData['active_nav_tab']=='admin-tab' ? "active" : "" !!}"
+                           wire:click="setActiveNavTab('admin-tab')">{{ trans('laratickets::admin.nav-administrator') }}</a>
+                    </li>
+                </ul>
+            </li>
     @endif
 </ul>
